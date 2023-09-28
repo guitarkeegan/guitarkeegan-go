46b58e056848bafa71aeffa598a6b4d32918e4d3
guitarkeegan <keegananglim@gmail.com>
2023-09-27 22:22:59 -0700

    get post request from contact form

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,14 @@ const cardEl = document.querySelector("#cardWrapper");
 const contactCardEl = document.querySelector("#contactWrapper");
 const flipCardInnerEl = document.querySelector(".flip-card-inner");
 const arrowWrapperEl = document.querySelector(".arrow-wrapper");
+const contactFormEl = document.querySelector("#contact-form");
+const emailEl = document.querySelector("#email");
+const messageEl = document.querySelector("#message");
 
 
 contactEl.addEventListener("click", cardFlip);
 arrowWrapperEl.addEventListener("click", cardUnflip);
+contactFormEl.addEventListener("submit", sendMessage);
 
 
 function cardFlip(){
@@ -19,4 +23,27 @@ function cardUnflip(){
     contactEl.disabled = false;
     contactEl.style.color = "#44e929";
     flipCardInnerEl.classList.remove("flipper");
+}
+
+function sendMessage(event){
+    event.preventDefault();
+
+    fetch("/api/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: (emailEl.value).trim(),
+            message: (messageEl.value).trim(),
+        })
+    })
+    .then(res => console.log(res))
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+
+    emailEl.value = "";
+    messageEl.value = "";
+
+    return
 }
