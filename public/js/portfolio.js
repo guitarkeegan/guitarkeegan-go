e42515f11217dd8f2a17389a932c32932fb29120
guitarkeegan <keegananglim@gmail.com>
2023-11-08 15:37:51 -0800

    add musical puzzel audio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -5,53 +5,57 @@ var faEl = document.querySelector(".fa");
 var fiEl = document.querySelector(".fi");
 var solEl = document.querySelector(".sol");
 var playButtonEl = document.querySelector("#playButton");
-
 var done = false;
+var beet = new Audio("/public/audio/bagatelle-25_01.mp3");
 
 playButtonEl.addEventListener("click", play);
-const unclickedColor = "rgb(200, 231, 195);"
+const unclickedColor = "rgb(200, 231, 195);";
 doEl.addEventListener("click", enterPorfolio);
 
 function delay() {
-    return new Promise((res, reg)=>{
-        setTimeout(()=>{
-            res(1)
-        }, 400);
-    });
+  return new Promise((res, reg) => {
+    setTimeout(() => {
+      res(1);
+    }, 210);
+  });
 }
 
 async function play() {
-    solEl.style.backgroundColor = "red";
-    console.log(await delay());
-    solEl.style.backgroundColor = "";
-    fiEl.style.backgroundColor = "red";
-    console.log(await delay());
-    fiEl.style.backgroundColor = "";
-    solEl.style.backgroundColor = "red";
-    console.log(await delay());
-    solEl.style.backgroundColor = "";
-    fiEl.style.backgroundColor = "red";
-    console.log(await delay());
-    fiEl.style.backgroundColor = "";
-    solEl.style.backgroundColor = "red";
-    console.log(await delay());
-    solEl.style.backgroundColor = "";
-    reEl.style.backgroundColor = "red";
-    console.log(await delay());
-    reEl.style.backgroundColor = "";
-    faEl.style.backgroundColor = "red";
-    console.log(await delay());
-    faEl.style.backgroundColor = "";
-    meEl.style.backgroundColor = "red";
-    console.log(await delay());
-    meEl.style.backgroundColor = "";
-    done = true;
+  beet.play();
+  solEl.style.backgroundColor = "red";
+  console.log(await delay());
+  solEl.style.backgroundColor = "";
+  fiEl.style.backgroundColor = "red";
+  console.log(await delay());
+  fiEl.style.backgroundColor = "";
+  solEl.style.backgroundColor = "red";
+  console.log(await delay());
+  solEl.style.backgroundColor = "";
+  fiEl.style.backgroundColor = "red";
+  console.log(await delay());
+  fiEl.style.backgroundColor = "";
+  solEl.style.backgroundColor = "red";
+  console.log(await delay());
+  solEl.style.backgroundColor = "";
+  reEl.style.backgroundColor = "red";
+  console.log(await delay());
+  reEl.style.backgroundColor = "";
+  faEl.style.backgroundColor = "red";
+  console.log(await delay());
+  faEl.style.backgroundColor = "";
+  meEl.style.backgroundColor = "red";
+  console.log(await delay());
+  meEl.style.backgroundColor = "";
+  reEl.style.backgroundColor = "red";
+  console.log(await delay());
+  reEl.style.backgroundColor = "";
+  done = true;
 }
 
-function enterPorfolio(){
-    console.log("entering portfolio...");
-    console.log(window.location.href);
-    if (done){
-        window.location.href = window.location.href + "/pass"
-    }
+function enterPorfolio() {
+  console.log("entering portfolio...");
+  console.log(window.location.href);
+  if (done) {
+    window.location.href = window.location.href + "/pass";
+  }
 }
